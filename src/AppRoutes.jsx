c45858460876750a06fd03430b4f1b95a4273549
Loruel <paco.jeansl@gmail.com>
2024-11-15 22:14:30 -0600

    arrer b2

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -4,12 +4,12 @@ import Home from './pages/Home'
 
 import Cruci from './pages/Cruci'
 import CruciB1 from './pages/CruciB1'
-import CruciB2 from './pages/cRUCIb2.JSX'
 import CruciH2 from './pages/CruciH2'
 import CruciH1 from './pages/CruciH1'
 import CruciRama from './pages/CruciRama'
 import CruciVenta from './pages/CruciVenta'
 import CruciMontes from './pages/CruciMontes'
+import CruB2 from './pages/CruB2'
 
 
 export default function AppRoutes() {
@@ -22,7 +22,7 @@ export default function AppRoutes() {
             <Route path='/h1' element={<CruciH1 />} />
             <Route path='/h2' element={<CruciH2 />} />
             <Route path='/b1' element={<CruciB1 />} />
-            <Route path='/b2' element={<CruciB2 />} />
+            <Route path='/b2' element={<CruB2 />} />
             <Route path='/rama' element={<CruciRama />} />
         </Routes>
     )
